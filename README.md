693d156b73e396beeed3d362c340de630a3bc6bb
Bernardo-Franco <85417001+Bernardo-Franco@users.noreply.github.com>
2023-08-09 13:07:36 -0500

    first commits - ready to deploy

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + Vite YouTube Clone APP
+
+[Link to the Deployed Site](https://bespoke-sopapillas-fb364d.netlify.app)
+
+this is a practice project taken from https://www.youtube.com/watch?v=FHTbsZEJspU&t=7156s - JSM
+
+Especial Thanks to adrianhajdin and all JSM team
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import ChannelDetail from './ChannelDetail'
+import Feed from './Feed'
+import NavBar from './NavBar'
+import SearchFeed from './SearchFeed'
+import VideoDetail from './VideoDetail'
+import Sidebar from './Sidebar'
+import Videos from './Videos'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
+
+export { ChannelDetail, Feed, NavBar, SearchFeed, VideoDetail, Sidebar, Videos, VideoCard, ChannelCard }
